081a6b68d0d6012783899e18c102b00119089272
Olb <braybilly@gmail.com>
2019-10-30 12:21:54 -0500

    Added log and check for pound

--- Source/OIDURLQueryComponent.m
+++ Source/OIDURLQueryComponent.m
@@ -50,6 +50,11 @@ static NSString *const kQueryStringParamAdditionalDisallowedCharacters = @"=&+";
             [NSURLComponents componentsWithURL:URL resolvingAgainstBaseURL:NO];
         // As OAuth uses application/x-www-form-urlencoded encoding, interprets '+' as a space
         // in addition to regular percent decoding. https://url.spec.whatwg.org/#urlencoded-parsing
+        NSLog(@"Components: %@", components.path);
+        if ([components.path containsString:@"#"]) {
+          NSLog(@"Containts #: try removing");
+          components.path = [components.path stringByReplacingOccurrencesOfString:@"#" withString:@""];
+        }
         components.percentEncodedQuery =
             [components.percentEncodedQuery stringByReplacingOccurrencesOfString:@"+"
                                                                       withString:@"%20"];
